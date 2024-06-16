a34f2d705565ed1f7f6a96197f8f0514cfebc297
SMA <sma@wizlinx.com>
2024-06-17 01:25:17 +0500

    client componet

---
+++ app/like-button.js
@@ -0,0 +1,13 @@
+"use client";
+
+import { useState } from "react";
+
+export default function LikeButton() {
+  const [likes, setLikes] = useState(0);
+
+  function handleClick() {
+    setLikes((l) => l + 1);
+  }
+
+  return <button onClick={handleClick}>Like ({likes})</button>;
+}
